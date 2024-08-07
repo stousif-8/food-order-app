5fc458e08a4f8a29d31d90fd83f4d7bebc30cc2b
SyedTousif <stousif1020@gmail.com>
2024-08-07 17:38:53 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_database():
-    client = MongoClient("mongodb://mongo:27017/")
+    client = MongoClient("mongodb+srv://admin:admin@my-app.5zs8j.mongodb.net/?retryWrites=true&w=majority&appName=my-app/tousif")
     return client["food_order_db"]
 
